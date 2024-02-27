143bf6300fc7d01ce28bd124961cf85523e6d704
weihongchen97 <weihongchen97@gmail.com>
2024-02-26 21:14:51 -0800

    Exercises

---
+++ Project: Library/app.js
@@ -0,0 +1,17 @@
+const addBook = document.addEventListener("#addBook");
+const myLibrary = [];
+//create a function for the book object
+function book(title, author, pages) {
+this.title = title;
+this.author = author;
+this.pages = pages;
+};
+function addBookToLibrary() {
+    myLibrary.push
+}
+addBook.addEventListener("click", addBookToLibrary);
+
+
+//create a button that turns into a form that has multiple inputs inside of it that will ask you to add a book
+
+//the book itself is going to be an object with multiple key value partitions but it's going to be stored in an array
