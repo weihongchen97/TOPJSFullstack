636307480457667eb312244c5c421c3e6e3a9e1b
weihongchen97 <weihongchen97@gmail.com>
2024-02-27 22:56:13 -0800

    updates

--- Project: Library/app.js
+++ Project: Library/app.js
@@ -10,6 +10,18 @@ function addBookToLibrary() {
     myLibrary.push
 }
 addBook.addEventListener("click", addBookToLibrary);
+const addBook = document.addEventListener("#addBook");
+const myLibrary = [];
+//create a function for the book object
+function book(title, author, pages) {
+this.title = title;
+this.author = author;
+this.pages = pages;
+};
+function addBookToLibrary() {
+    myLibrary.push
+}
+addBook.addEventListener("click", addBookToLibrary);
 
 
 //create a button that turns into a form that has multiple inputs inside of it that will ask you to add a book
